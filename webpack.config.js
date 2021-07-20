51423b17564d70c65362c50301dc87e23237a786
ridaarif98 <ridaarif16@gmail.com>
2021-07-20 11:33:20 +0200

    add style for index.html

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,23 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+//   entry: './src/index.js',
+   entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'main.js',
+    // filename: 'main.js',
+    // filename:'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
